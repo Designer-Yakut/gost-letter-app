b10e3bea27d9242abd1fe88a1af83cc1143077ec
Designer-Yakut <0250113dgmail.com>
2025-11-27 19:22:08 +0300

    Корректировка текста-образца в app.py

--- app.py
+++ app.py
@@ -177,11 +177,11 @@ HTML_FORM = """
     <p>
     <label>Введите текст ниже или выберите файл:</label>
     <span style="font-size: 0.8em; color: #FF8C00; margin-left: 8px;">
-        *Мин. строк и символов для Render, зависит от h
+           *Мин. строк и символов для Render, зависит от h
       </span>
     <p>
     <textarea name="text" rows="6" cols="60">Автор
-    Yakutsenak 2025</textarea>
+Yakutsenak 2025</textarea>
 
     Загрузить текстовый файл (.txt):
     <input type="file" name="textfile">
